5bcdd2a7dec629637e58d3ca3278ad77d8d23698
Randula Ariyawanse <randula.j.ariyawanse@gmail.com>
2024-08-20 19:02:30 +1000

    added documents dump from dev

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from .views import ChatBot, get_documents
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path("chatbot", ChatBot.as_view(), name="chatbot_view"),
     path("documents", get_documents, name="get_documents"),
@@ -10,4 +15,5 @@ urlpatterns = [
         ChatBot.delete_chat_history,
         name="delete_chathistory",
     ),
+    path("sentry-debug/", trigger_error),
 ]
