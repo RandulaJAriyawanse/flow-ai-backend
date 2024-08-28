59d74eee2151ea600164c8799276aa96a18ee957
Randula Ariyawanse <randula.j.ariyawanse@gmail.com>
2024-08-28 22:09:46 +1000

    debug false

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-peu42+4xi@ha(#(r=&g!_d#dund4_w$=g)w@b*(%-uk)kct5q$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
